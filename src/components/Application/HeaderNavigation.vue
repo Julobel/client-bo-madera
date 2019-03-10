85a2a132105d30b0590eb99c14fbeb444fec8f47
Jules Aubel <jules.aubel@protonmail.com>
2019-03-11 00:19:03 +0100

    Init project

---
+++ src/components/Application/HeaderNavigation.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <v-navigation-drawer
+                clipped
+                fixed
+                v-model="drawer"
+                app
+        >
+            <v-list dense>
+                <v-list-tile @click="">
+                    <v-list-tile-action>
+                        <v-icon>dashboard</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                        <v-list-tile-title>Devis</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+            </v-list>
+        </v-navigation-drawer>
+        <v-toolbar class="green darken-4" app fixed clipped-left>
+            <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+            <v-toolbar-title class="headline text-uppercase">
+                <span class="mr-2 white--text">Madera</span>
+                <span class="font-weight-light grey--text text--lighten-4">BACK OFFICE</span>
+            </v-toolbar-title>
+        </v-toolbar>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HeaderNavigation",
+        data: () => ({
+            drawer: false
+        }),
+    }
+</script>
+
+<style scoped>
+
+</style>
